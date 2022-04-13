a18d232a4e065bad8e6e8af67d423ae7e75a4080
Marshan Eric <marshansr@gmail.com>
2022-04-14 00:32:53 +0300

    fix phpcs errors

--- public/index.php
+++ public/index.php
@@ -6,10 +6,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Src\Anagram\Sentence;
 
-function makeAnagram(string $input): string
-{
-    $sentence = new Sentence($input);
-    return $sentence->getReversed();
-}
-
-echo makeAnagram('A1bcd efg!h');
+echo (new Sentence($input))->getReversed();
